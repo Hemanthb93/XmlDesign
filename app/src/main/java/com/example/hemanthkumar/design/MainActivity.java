f181f4d110d55e1a9185ebd033fb217bd5a40240
hemanthb93 <hemanthkumar@mirafra.com>
2018-04-17 14:03:00 +0530

    first commit

---
+++ app/src/main/java/com/example/hemanthkumar/design/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hemanthkumar.design;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = findViewById(R.id.tv_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
